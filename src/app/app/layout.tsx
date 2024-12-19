772ab0b2204edf97cd8da134b2b84037faa52dd6
aandrewsv <agustin.andrews@gmail.com>
2024-12-19 16:28:32 -0300

    feat: implement authentication check on app layout and improve error message for missing token

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,11 +1,24 @@
 // src/app/app/layout.tsx
+"use client"
 import { Header } from "@/components/app/header"
+import { useAuth } from "@/contexts/auth-context";
+import { useEffect } from "react";
 
 export default function AppLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const { checkAuthAndRedirect, isCheckingAuth } = useAuth();
+
+  useEffect(() => {
+    checkAuthAndRedirect();
+  }, []);
+
+  if (isCheckingAuth) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="min-h-screen flex flex-col bg-gray-900">
       <Header />
